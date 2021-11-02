b81e7f7a7d7eeef033cbf4968521ab6e950df543
David Wright <david.wright@smoothstack.com>
2021-11-02 18:27:28 -0500

    Add assignment

---
+++ Java Basics 2/org/javabasics/two/assignments/one/Main.java
@@ -0,0 +1,22 @@
+package org.javabasics.two.assignments.one;
+
+public class Main {
+  public static void main(String[] args) {
+    var total = 0;
+
+    for (var argument : args)
+      total += parseInput(argument).intValue();
+
+    System.out.println("The total is: " + total);
+  }
+
+  private static Integer parseInput(String input) {
+    try {
+      return Integer.parseInt(input);
+    } catch (Exception e) {
+      System.out.println(input + " is not a valid number. Please make sure all arguments are whole numbers");
+      System.exit(0);
+      return 0;
+    }
+  }
+}
