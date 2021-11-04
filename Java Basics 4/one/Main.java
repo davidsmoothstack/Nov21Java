facb4a7a5a02e6e8ffd14cc6843c742868c8eade
David Wright <david.wright@smoothstack.com>
2021-11-04 15:29:03 -0500

    Update Assignment

--- Java Basics 4/one/Main.java
+++ Java Basics 4/one/Main.java
@@ -1,18 +1,28 @@
 package one;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
   private volatile static Main instance;
 
   public static void main(String[] args) {
-    var x = Main.getInstance();
-    System.out.println(x.hashCode());
+    var threadList = Arrays.asList(new Thread[10]);
+
+    threadList.stream()
+        .map(t -> new Thread(
+            () -> {
+            }))
+        .parallel()
+        .forEach(t -> t.start());
   }
 
   public static Main getInstance() {
     if (instance == null) {
       synchronized (Main.class) {
-        if (instance == null)
+        if (instance == null) {
           instance = new Main();
+        }
       }
     }
 
