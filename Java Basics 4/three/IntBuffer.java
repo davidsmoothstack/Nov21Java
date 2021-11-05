fee6523923a243f98f50d3dddc2eb9593589e063
David Wright <david.wright@smoothstack.com>
2021-11-04 20:25:56 -0500

    Implement Iterable instead of Collection

--- Java Basics 4/three/IntBuffer.java
+++ Java Basics 4/three/IntBuffer.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
-public class IntBuffer implements Collection<Integer> {
+public class IntBuffer implements Iterable<Integer> {
   private Collection<Integer> intCollection;
   public Integer maxSize;
 
@@ -18,22 +18,18 @@ public class IntBuffer implements Collection<Integer> {
     return intCollection.size() == maxSize;
   }
 
-  @Override
   public int size() {
     return intCollection.size();
   }
 
-  @Override
   public boolean isEmpty() {
     return intCollection.size() == 0;
   }
 
-  @Override
   public boolean add(Integer e) {
     return intCollection.add(e);
   }
 
-  @Override
   public boolean remove(Object o) {
     return intCollection.remove(o);
   }
@@ -41,52 +37,5 @@ public class IntBuffer implements Collection<Integer> {
   @Override
   public Iterator<Integer> iterator() {
     return intCollection.iterator();
-  }
-
-  @Override
-  public boolean contains(Object o) {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  @Override
-  public Object[] toArray() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public <T> T[] toArray(T[] a) {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public boolean containsAll(Collection<?> c) {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  @Override
-  public boolean addAll(Collection<? extends Integer> c) {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  @Override
-  public boolean removeAll(Collection<?> c) {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  @Override
-  public boolean retainAll(Collection<?> c) {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  @Override
-  public void clear() {
-    // TODO Auto-generated method stub
   }
 }
