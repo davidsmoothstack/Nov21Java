65eb5b499f0c926cb7c5d20e48ede9bcd0e18cb7
David Wright <david.wright@smoothstack.com>
2021-11-05 03:11:49 -0500

    Add Assignment

---
+++ Java Basics 4/four/tests/LineTests.java
@@ -0,0 +1,48 @@
+package four.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import four.Line;
+
+public class LineTests {
+    private Line sut;
+
+    @Before
+    public void beforeEach() {
+        sut = new Line(0, 0, 2, 4);
+    }
+
+    @Test
+    public void getSlope_returns_correctSlope() {
+        assertEquals(2, sut.getSlope(), 0.0001);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void getSlope_throws_whenX1EqualsX0() {
+        sut = new Line(0, 0, 0, 0);
+        sut.getSlope();
+    }
+
+    @Test
+    public void getDistance_returns_correctDistance() {
+        assertEquals(4.47213595499958, sut.getDistance(), 0.0001);
+    }
+
+    @Test
+    public void parallelTo_returns_true_whenLinesAreParallel() {
+        sut = new Line(1, 2, 3, 4);
+        var parallel = new Line(2, 4, 6, 8);
+
+        assertEquals(true, sut.parallelTo(parallel));
+    }
+
+    @Test
+    public void parallelTo_returns_false_whenLinesAreNotParallel() {
+        var notParallel = new Line(10, 20, 30, 40);
+
+        assertEquals(false, sut.parallelTo(notParallel));
+    }
+}
