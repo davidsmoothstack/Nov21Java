efaf29080c76fc438841bff6cfb23a9caf4abf5e
David Wright <david.wright@smoothstack.com>
2021-11-05 21:43:50 -0500

    Create a method

--- Java Basics 5/lambdas/QuestionTwo.java
+++ Java Basics 5/lambdas/QuestionTwo.java
@@ -1,7 +1,7 @@
 package lambdas;
 
-
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Random;
 import java.util.stream.Collectors;
 
@@ -9,11 +9,16 @@ public class QuestionTwo {
     static Random rng = new Random();
 
     public static void main(String[] args) {
-        var output = Arrays.stream(new Integer[30])
-                .map(i -> rng.nextInt(99))
-                .map(i -> i % 2 == 0 ? "e" + i : "o" + i)
-                .collect(Collectors.joining(","));
+        var intCollection = Arrays.stream(new Integer[30]).map(i -> rng.nextInt(99)).toList();
+        var output = generateIntString(intCollection);
 
         System.out.println(output);
+    }
+
+    public static String generateIntString(Collection<Integer> collection) {
+
+        return collection.stream()
+                .map(i -> i % 2 == 0 ? "e" + i : "o" + i)
+                .collect(Collectors.joining(","));
     }
 }
