ef02f054b897dd79a60dd2881148210450863d2f
David Wright <david.wright@smoothstack.com>
2021-11-05 21:37:54 -0500

    Create a function

--- Java Basics 5/lambdas/QuestionThree.java
+++ Java Basics 5/lambdas/QuestionThree.java
@@ -1,13 +1,23 @@
 package lambdas;
 
 import java.util.Arrays;
+import java.util.Collection;
 
 public class QuestionThree {
     public static void main(String[] args) {
-        Arrays.stream("All the apples are named Ann or Abe".split("\s+"))
+        var result = getQualifiedElements("All the apples are named Ann or Abe");
+
+        for (var item : result) {
+            System.out.println(item);
+        }
+    }
+
+    // Qualified elements must start with the letter "a" and be 3 characters long
+    public static Collection<String> getQualifiedElements(String input) {
+        return Arrays.stream(input.split("\s+"))
                 .map(word -> word.toLowerCase())
                 .filter(word -> word.charAt(0) == 'a')
                 .filter(word -> word.length() == 3)
-                .forEach(word -> System.out.println(word));
+                .toList();
     }
 }
