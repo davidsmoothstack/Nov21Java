150e4418da0c06be93165c98eb1a2170780a2a98
David Wright <david.wright@smoothstack.com>
2021-11-07 15:44:09 -0600

    Add assessments

--- Week 1 Assesment/assignment/two/Assessment.java
+++ Week 1 Assesment/assignment/two/Assessment.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 public class Assessment {
     public static Collection<Integer> getRightMostDigit(Collection<Integer> numbers) {
         return numbers.stream()
-                .map(n -> n % 10).toList();
+                .map(n -> n % 10)
+                .toList();
     }
 }
